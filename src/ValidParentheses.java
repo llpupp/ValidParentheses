afdcf72aa56afebcf0069654a120ad6225729280
llpupp <llpupp@hotmail.com>
2022-12-01 15:49:18 -0300

    Inclusão Readme

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,5 +1,34 @@
-public class Main {
+import java.util.Scanner;
+
+public class ValidParentheses {
+
+    private static boolean a = true;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean character = isValid(scan.nextLine());
+
+        System.out.println(character);
+
+
+    }
+
+    public static boolean isValid(String b) {
+
+        char firstCharacter = b.charAt(0);
+        char lastCharacter = b.charAt(b.length() - 1);
+
+        if (firstCharacter == '(' && lastCharacter == ')') {
+            a = true;
+        } else if (firstCharacter == '[' && lastCharacter == ']') {
+            a = true;
+        } else {
+            a = false;
+        }
+        return a;
+
     }
 }
+
